e5f15c012c52b9e0f9d1c25118b7916499c400cd
Steve Kinney <hello@stevekinney.net>
2013-04-12 11:01:01 -0400

    Graph out dummy data in an SVG

--- public/javascripts/_visualization.js
+++ public/javascripts/_visualization.js
@@ -1,25 +1,41 @@
 // Create an SVG
-var w = 400, h = 200;
+var w = 500, h = 100, barPadding = 1;
 
-d3.select('body')
+var svg = d3.select('body')
   .append('svg')
   .attr('width', w)
   .attr('height', h);
 
 // Pull in some data from a CSV
-d3.csv('/data/dummy-data.csv', function(error, data) {
+d3.csv('/data/dummy-data.csv', function(error, dataset) {
   
   if (error) {
     console.log(error);
     return;
   }
   
+  var maxValue = h, minValue = h;
+  for (var i = dataset.length - 1; i >= 0; i--){
+    if ( dataset[i].Data > maxValue ) maxValue = dataset[i].Data;
+    if ( dataset[i].Data < minValue ) minValue = dataset[i].Data;
+  };
+  
   // Append some text to the screen based on the data
-  d3.select('body')
-    .selectAll('p')
-    .data(data)
+  svg.selectAll('rect')
+    .data(dataset)
     .enter()
-    .append('p')
-    .text(function (row) { return "Year: " + row.Year + " (" + row.Data + ")"});
+    .append('rect')
+    .attr('x', function (d, i) {
+      return i * (w / dataset.length);
+    })
+    .attr('y', function (d, i) {
+      return 0;
+    })
+    .attr('width', function (d, i) {
+      return w / dataset.length - barPadding;
+    })
+    .attr('height', function (d, i) {
+      return (d.Data - minValue) / ( maxValue - minValue ) * h;
+    });
 
 });
